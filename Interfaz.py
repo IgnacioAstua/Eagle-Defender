c3818eb904ff4e0796baaa59c45486cc6e7860af
IgnacioAstuaR <iastua@estudiantec.cr>
2023-10-03 09:41:07 -0600

    Cambios en la interfaz

--- Interfaz.py
+++ Interfaz.py
@@ -46,7 +46,7 @@ entrada_correo = tk.Entry(registro, font= "Fixedsys 25", bg= 'grey', width=15, r
 entrada_correo.place(x=100, y=315)
 contrasena_txt = tk.Label(registro, text= 'Contraseña:', font= 'Fixedsys 25', bg='grey', fg='black', relief= 'raised')
 contrasena_txt.place(x=140, y=375)
-entrada_contrasena = tk.Entry(registro, font= "Fixedsys 25", bg= 'grey', width=15, relief="sunken")
+entrada_contrasena = tk.Entry(registro, font= "Fixedsys 25", bg= 'grey', width=15, relief="sunken", show= '*')
 entrada_contrasena.place(x=100, y=435)
 edad_txt = tk.Label(registro, text= 'Edad:', font= 'Fixedsys 25', bg='grey', fg='black', relief= 'raised')
 edad_txt.place(x=595, y=15)
@@ -61,6 +61,32 @@ redes.place(x=470, y=305)
 
 #Ventana de ajustes
 ajustes_canva = tk.Canvas(ventana1, width=664, height=465, bg='grey')
+musica_txt = tk.Label(ajustes_canva, text= 'Música', font= 'Fixedsys 25', bg='grey', fg='black', relief= 'raised')
+musica_txt.place(x=275, y=25)
+mute = tk.Button(ajustes_canva, text='No', font= 'Fixedsys 20',bg='grey', fg='black')
+mute.place(x=400,y=80)
+unmute = tk.Button(ajustes_canva, text='Sí', font= 'Fixedsys 20',bg='grey', fg='black')
+unmute.place(x=220, y=80)
+volumen_txt = tk.Label(ajustes_canva, text= 'Volumen', font= 'Fixedsys 25', bg='grey', fg='black', relief= 'raised')
+volumen_txt.place(x=270, y=175)
+menos = tk.Button(ajustes_canva, text='-', font= 'Fixedsys 20',bg='grey', fg='black')
+menos.place(x=420,y=230)
+mas = tk.Button(ajustes_canva, text='+', font= 'Fixedsys 20',bg='grey', fg='black')
+mas.place(x=220, y=230)
+idioma_txt = tk.Label(ajustes_canva, text= 'Idioma', font= 'Fixedsys 25', bg='grey', fg='black', relief= 'raised')
+idioma_txt.place(x=285, y=325)
+espanol = tk.Button(ajustes_canva, text='Español', font= 'Fixedsys 20',bg='grey', fg='black')
+espanol.place(x=400,y=380)
+ingles = tk.Button(ajustes_canva, text='English', font= 'Fixedsys 20',bg='grey', fg='black')
+ingles.place(x=150, y=380)
+
+#Ventana salon de la fama
+salon_canva = tk.Canvas(ventana1, width=700, height=447)
+fondo3 = tk.PhotoImage(file='pared.png')
+fondo_salon = tk.Label(salon_canva, image=fondo3)
+fondo_salon.place(x=0, y=0)
+mejores_txt = tk.Label(salon_canva, text= 'Mejores Puntuaciones', font= 'Fixedsys 25',bg='grey', fg='black', relief='raised')
+mejores_txt.place(x=165, y=20)
 
 #Titulo principal
 tituloP = tk.Label (canva1, text = "Eagle Defender", font = "Fixedsys 80 ",bg= "grey", fg='black', relief= 'raised')
@@ -79,13 +105,17 @@ salir_registro = tk.Button(registro, text = "Volver", font = "Fixedsys 16",bg='g
 salir_registro.place(x=10, y=10)
 
 #Boton salon de la fama
-salon_fama = tk.Button(canva1, text='Salón de la fama', font= 'Fixedsys 25',bg='grey', fg='black')
+salon_fama = tk.Button(canva1, text='Salón de la fama', font= 'Fixedsys 25',bg='grey', fg='black', command= lambda: salon_canva.pack(side=tk.TOP, pady=100))
 salon_fama.place(x=590, y=500)
+salir_salon = tk.Button(salon_canva, text = "Volver", font = "Fixedsys 16",bg='grey', fg='black', command = lambda: salon_canva.pack_forget())
+salir_salon.place(x=10, y=10)
+
 #Boton de ajustes
 ajust=tk.Button(canva1, text ="Ajustes", font ="Fixedsys 25", bg='grey', fg='black', command= lambda: ajustes_canva.pack(side= tk.TOP, pady=50))
 ajust.place(x=680, y=600)
-salir_ajustes=tk.Button(ajustes_canva, text= 'Volver', font= 'Fixedsys 16', bg='black',fg='white', command= lambda: ajustes_canva.pack_forget())
+salir_ajustes=tk.Button(ajustes_canva, text= 'Volver', font= 'Fixedsys 16', bg='grey',fg='black', command= lambda: ajustes_canva.pack_forget())
 salir_ajustes.place(x=10,y=10)
+
 #Boton de salida
 salirP=tk.Button(canva1, text = "Salir", font = "Fixedsys 16",bg='grey', fg='black', command = lambda: ventana1.destroy())
 salirP.place(x=20,y=10)
